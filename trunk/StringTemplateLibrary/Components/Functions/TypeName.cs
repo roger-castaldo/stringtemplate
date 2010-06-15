22c5f1a20a53e65dbcca2603fbca06b4339e5e55
roger.castaldo@gmail.com <roger.castaldo@gmail.com@f79fb63d-9fae-b4d1-0816-cef9e18b62e2>
2010-06-15 00:45:12 +0000

    added in function to extract type name from an object.

---
+++ trunk/StringTemplateLibrary/Components/Functions/TypeName.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Org.Reddragonit.Stringtemplate.Tokenizers;
+
+namespace Org.Reddragonit.Stringtemplate.Components.Functions
+{
+    public class TypeName : FunctionComponent
+    {
+        private static readonly Regex _reg = new Regex("^[T|t][Y|y][P|p][E|e][N|n][A|a][M|m][E|e]\\(([V|v][A|a][R|r]\\s*=\\s*)?(.+)\\)$", RegexOptions.Compiled | RegexOptions.ECMAScript);
+
+        protected override string FunctionName
+        {
+            get { return "TypeName"; }
+        }
+
+        protected override string[] FunctionParameters
+        {
+            get { return new string[] { "var" }; }
+        }
+
+        private string val;
+
+        public override bool Load(Queue<Org.Reddragonit.Stringtemplate.Tokenizers.Token> tokens, Type tokenizerType, TemplateGroup group)
+        {
+            Token t = tokens.Dequeue();
+            val = _reg.Match(t.Content).Groups[2].ToString();
+            return true;
+        }
+
+        public override string GenerateString(ref Dictionary<string, object> variables)
+        {
+            object obj = Utility.LocateObjectInVariables(val, variables);
+            if (obj == null)
+                throw new Exception("Unable to locate a variable of the name " + val + " in order to extract the type name");
+            return obj.GetType().FullName;
+        }
+
+        public override Org.Reddragonit.Stringtemplate.Interfaces.IComponent NewInstance()
+        {
+            return new TypeName();
+        }
+    }
+}
